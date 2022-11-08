027215cfdade6f5201daf92f4110ae0f22cf99d0
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-11-08 08:28:32 +0100

    add commentary (definition)

--- model/entities/Category.php
+++ model/entities/Category.php
@@ -1,15 +1,17 @@
 <?php
+    // Les namespaces permettent de définir un nom de "package" que l'on pourra charger de manière automatique (autoloading)
     namespace Model\Entities;
 
     use App\Entity;
 
+    //  le mot clé "final pour" les classes empêche l'heritage
     final class Category extends Entity{
 
         private $id;
         private $label;
         
-        public function __construct($data){         
-            $this->hydrate($data);        
+        public function __construct($data){ 
+        // hydrater un objet permet de fournir des valeurs à ses attributs. La fonction prend comme argument, un tableau associatif ($data)
         }
  
 // GETTER ID
