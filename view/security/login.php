7313017de209f6ea8f2c212fe1708c50ecc5118b
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-11-13 23:32:43 +0100

    add link to stylesheet

--- view/security/login.php
+++ view/security/login.php
@@ -4,24 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\css/style-connect.css">
+    <link rel="stylesheet" href="../../public/css/style-connect.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <title>Se connecter</title>
 </head>
 <body class="body-connect">
-    <h1>Se connecter</h1>
-
-    <form action="\exercices/forum-PHP/index.php?ctrl=security&action=login" method="post">
-    <label >
-            email : <br>
-            <input type="text" name="email" required>
-        </label>
-        <br>
+    <header>
+        <a href="/exercices/Forum-PHP/"><i class="fa-solid fa-house"></i>Accueil</a>
+    </header>
+    <main>
+        <h1>Se connecter</h1>
+        <!-- FORMULAIRE -->
+        <form action="\exercices/forum-PHP/index.php?ctrl=security&action=login" method="post">
         <label >
-            password : <br>
-            <input type="password" name="password" required>
-        </label>
-        <br>
-        <input type="submit" value="Se connecter">
-    </form>
+                email : <br>
+                <input type="text" name="email" required>
+            </label>
+            <br>
+            <label >
+                password : <br>
+                <input type="password" name="password" required>
+            </label>
+            <br>
+            <input type="submit" value="Se connecter">
+        </form>
+    </main>
+
 </body>
 </html>
--- view/security/register.php
+++ view/security/register.php
@@ -4,35 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\css/style-register.css">
+    <link rel="stylesheet" href="../../public/css/style-register.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <title>S'enregistrer</title>
 </head>
 <body>
-    <h1>S'inscrire</h1>
-
-    <form action="\exercices/forum-PHP/index.php?ctrl=security&action=addUser" onsubmit="validation()" method="post">
-        <label >
-            pseudonyme : <br>
-            <input type="text" name="pseudonyme" required>
-        </label>
-        <br>
-        <label >
-            email : <br>
-            <input type="email" name="email" required>
-        </label>
-        <br>
-        <label >
-            Mot de passe : <br>
-            <input type="text" name="password" required>
-        </label>
-        <br>
-        <label >
-            confirmer le mot de passe : <br>
-            <input type="text" name="password2" required>
-        </label>
-        <br>
-        <input type="submit" value="S'inscrire">
-    </form>
+    <header>
+        <a href="/exercices/Forum-PHP/"><i class="fa-solid fa-house"></i>Accueil</a>
+     </header>
+    <main>
+        <h1>S'inscrire</h1>
+    
+        <form action="\exercices/forum-PHP/index.php?ctrl=security&action=addUser" onsubmit="validation()" method="post">
+            <p>
+                <label >
+                    pseudonyme : <br>
+                </label>
+                <input type="text" name="pseudonyme" required>
+            </p>
+            <p>
+                <label >
+                    email : <br>
+                    <input type="email" name="email" required>
+                </label>
+            </p>
+            <p>
+                <label >
+                    Mot de passe : <br>
+                    <input type="text" name="password" required>
+                </label>
+            </p>
+            <p>
+                <label >
+                    confirmer le mot de passe : <br>
+                    <input type="text" name="password2" required>
+                </label>
+            </p>
+            <p>
+                <input id="button" type="submit" value="S'inscrire">
+            </p>
+        </form>
+    </main>
 
     <!-- message de validation -->
     <script>
